8e324c129ced60496b4a43fd06fa526a6ac2efea
firstaru <firstarchive1@outlook.com>
2023-12-08 06:21:07 +0700

    resize /Home review profile

--- data/review.ts
+++ data/review.ts
@@ -1,7 +1,6 @@
 export const reviews = [
   {
-    avatar:
-      "https://scontent.fbkk30-1.fna.fbcdn.net/v/t39.30808-6/241993548_2532787876865590_2701806814492897269_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=9c7eae&_nc_eui2=AeHsz8PgYxBwSM8fY7XoUd6oz2XYjUVvrcLPZdiNRW-twjhH4jMAitm0QFyfM9dwgcaThkyfPrDxB-KiKPo05ZLa&_nc_ohc=kWLY2HreGawAX82W_Hi&_nc_ht=scontent.fbkk30-1.fna&oh=00_AfC2VZMe6IcwksZlrKKmZnBWyDJCzgb_bY1R1Ekz9d3axw&oe=6575E6F8",
+    avatar: "https://img5.pic.in.th/file/secure-sv1/4_2_11zon.th.webp",
     rating: 5,
     comment:
       "มีความประทับใจมากที่สวนของฉันเปลี่ยนไปได้เต็มที่กับหญ้าเทียมนี้! เป็นการตกแต่งที่ยอดเยี่ยม และมีความสมจริงที่ไม่น่าเชื่อ ทุกคนต้องลอง! #สวนในฝัน #หญ้าเทียมมหัศจรรย์",
@@ -9,8 +8,7 @@ export const reviews = [
     time: "2017, 0, 1",
   },
   {
-    avatar:
-      "https://scontent.fbkk30-1.fna.fbcdn.net/v/t39.30808-6/340745428_636947991547497_9036922299170814505_n.jpg?stp=cp6_dst-jpg&_nc_cat=111&ccb=1-7&_nc_sid=efb6e6&_nc_eui2=AeFu16-tRd3UQgYWfoiUguTObWldbaQKW19taV1tpApbX5Q05ZGjgDSXzhMb0-RQ7UhUfr1p2yJICa_zP2oojLlZ&_nc_ohc=4CeqKDRSl0oAX_cstnY&_nc_ht=scontent.fbkk30-1.fna&oh=00_AfBwm8YLZgl9X915ZAmQ86lsPy5EmRMwZ6BlejupW2dLmw&oe=6576E063",
+    avatar: "https://img2.pic.in.th/pic/5_1_11zon.th.webp",
     rating: 4,
     comment:
       "การเลือกใช้หญ้าเทียมทำให้บรรยากาศในบ้านเราดูดีขึ้นมาก! มันไม่แค่สวยงาม แต่ยังทำให้รู้สึกสบายใจมากขึ้น หญ้าที่ทำให้บ้านเราเป็นที่ที่สุขสงบ แน่นอนครับ! #บ้านสวนเรา #รีวิวหญ้าเทียม",
@@ -18,8 +16,7 @@ export const reviews = [
     time: "2022, 0, 1",
   },
   {
-    avatar:
-      "https://scontent.fbkk30-1.fna.fbcdn.net/v/t1.6435-9/127163867_2271295229681524_82606257333996543_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=7a1959&_nc_eui2=AeGOnZaev7BV5IAbqSZnXICq6CpzZUrp0eXoKnNlSunR5ZELMmmxYi-mWA5diAq13tQnP6pEfxYfrFx9ICwngmu1&_nc_ohc=3sdBkMqlrSwAX-lRKLv&_nc_ht=scontent.fbkk30-1.fna&oh=00_AfCYjsMecUKcHOVFm6M7odR6dnnHC6Fioa3xB3oM2mWRxA&oe=659897B4",
+    avatar: "https://img2.pic.in.th/pic/6_6_11zon.th.webp",
     rating: 4,
     comment:
       "ความสวยงามของหญ้าเทียมนี้ทำให้หลงรักและติดตามมันมาทุกที คุณภาพดีทุกด้าน รู้สึกประทับใจมาก และไม่เคยผิดหวัง! #รักหญ้าเทียม #บ้านสวนสุขภาพ",
@@ -27,8 +24,7 @@ export const reviews = [
     time: "2020, 0, 1",
   },
   {
-    avatar:
-      "https://scontent.fbkk30-1.fna.fbcdn.net/v/t1.6435-9/122462927_2237926943018353_6462277315545544079_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=7a1959&_nc_eui2=AeFbtDanNzV6DclBWHyXZD49ACeV7wz5EJkAJ5XvDPkQmbR9NrdzGXor1T9XopXcTwC5WVzwi968JcCz4Cgq3g0M&_nc_ohc=oCEVGejjwiMAX_VUs1T&_nc_ht=scontent.fbkk30-1.fna&oh=00_AfChMdQpE27GvixqibUkqqej4Ku2c_Jf9wvZ3N2fGH_gFw&oe=6598AA50",
+    avatar: "https://img2.pic.in.th/pic/1_5_11zon.th.webp",
     rating: 2,
     comment:
       "หญ้าเทียมช่วยให้สวนของฉันดูน่าอยู่มากขึ้น มันไม่แค่สวยงาม แต่ยังทำให้บ้านมีความสะอาดมากขึ้น รักมากๆ! #สวนสะอาด #หญ้าเทียมคือคำตอบ",
@@ -36,8 +32,7 @@ export const reviews = [
     time: "2021, 0, 1",
   },
   {
-    avatar:
-      "https://scontent.fbkk30-1.fna.fbcdn.net/v/t1.6435-9/121151516_2222118521265862_7374994049747079642_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=7a1959&_nc_eui2=AeH4bifuljwrm3q6L5AAhnH3C6iyWyiRIgALqLJbKJEiALQZFYZNBpzQ0FQEAhVlLG52PVOs08KPGWERh9Ssn98M&_nc_ohc=065Lqs4bj08AX9nIYuV&_nc_ht=scontent.fbkk30-1.fna&oh=00_AfDwYofX-_Dj65pt-mI2jmgMxIvpicGCCrCt2DiFExqhBQ&oe=659881AC",
+    avatar: "https://img5.pic.in.th/file/secure-sv1/2_4_11zon.th.webp",
     rating: 3,
     comment:
       "หญ้าเทียมนี้ทำให้รู้สึกเหมือนอยู่ในสวนที่สวยงามทุกวัน เป็นเรื่องที่สุดท้ายที่ฉันต้องกังวลเลย มันเหมาะสำหรับทุกคนที่ต้องการสวนที่งดงาม และที่ทำให้เรารู้สึกสบายใจ! #รีวิวหญ้าเทียม #สวนในบ้าน",
